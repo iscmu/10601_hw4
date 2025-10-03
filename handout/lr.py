8c2fce8788007046fc3fd4f0ff721af994f26f6c
Iskander Sergazin <iskandersergazin@Iskanders-MacBook-Pro-3.local>
2025-10-03 18:36:49 -0400

    desired metric results are produced

--- handout/lr.py
+++ handout/lr.py
@@ -1,6 +1,11 @@
 import numpy as np
 import argparse
+# from feature import load_tsv_dataset
 
+def load_tsv_dataset(file):
+    dataset = np.loadtxt(file, delimiter='\t', comments=None, encoding='utf-8',
+                         dtype='float')
+    return dataset
 
 def sigmoid(x : np.ndarray):
     """
@@ -25,15 +30,20 @@ def train(
     learning_rate : float
 ) -> None:
     # TODO: Implement `train` using vectorization
-    pass
-
+    for e in range(num_epoch):
+        # theta -= learning_rate * (-(y - sigmoid( X @ theta)) @ X)
+        for i in range(len(X)):
+            arg = X[i] @ theta
+            theta -= learning_rate * ((-(y[i] - sigmoid( arg )) * X[i]))
+    
 
 def predict(
-    theta : np.ndarray,
-    X : np.ndarray
+    theta : np.ndarray, # shape (D,) where D is feature dim
+    X : np.ndarray      # shape (N, D) where N is num of examples
 ) -> np.ndarray:
     # TODO: Implement `predict` using vectorization
-    pass
+    # pass
+    return sigmoid(X @ theta)
 
 
 def compute_error(
@@ -41,7 +51,7 @@ def compute_error(
     y : np.ndarray
 ) -> float:
     # TODO: Implement `compute_error` using vectorization
-    pass
+    return 1 - ((y_pred >= 0.5) == y).sum() / len(y_pred)
 
 
 if __name__ == '__main__':
@@ -60,3 +70,53 @@ if __name__ == '__main__':
     parser.add_argument("learning_rate", type=float,
                         help='learning rate for stochastic gradient descent')
     args = parser.parse_args()
+
+    data_train = load_tsv_dataset(args.train_input)
+    data_test = load_tsv_dataset(args.test_input)
+    data_val = load_tsv_dataset(args.validation_input)
+
+    # Training
+    X = data_train[:,1:]
+    y = data_train[:, 0]
+
+    # Folding the intercept vector
+    theta = np.zeros(X.shape[1] + 1, dtype=float)
+    X = np.pad(X, ((0, 0), (0, 1)), mode='constant', constant_values=1)
+
+    train(theta, X, y, args.num_epoch, args.learning_rate)
+    y_pred = predict(theta, X)
+    tr_err = compute_error(y_pred, y)
+
+    # Validation
+    Xval = data_val[:, 1:]
+    yval = data_val[:, 0]
+    Xval = np.pad(Xval, ((0, 0), (0, 1)), mode='constant', constant_values=1)
+    y_pred_val = predict(theta, Xval)
+    val_err = compute_error(y_pred_val, yval)
+
+    # Test
+    Xtest = data_test[:,1:]
+    ytest = data_test[:, 0]
+    Xtest = np.pad(Xtest, ((0, 0), (0, 1)), mode='constant', constant_values=1)
+    y_pred_test = predict(theta, Xtest)
+    test_err = compute_error(y_pred_test, ytest)
+
+    def write_result(f, labels):
+        for i in labels:
+            f.write(str(int(i)) + '\n')
+
+    with open(args.train_out, 'w') as f:
+        write_result(f, y_pred)
+    with open(args.test_out, 'w') as f:
+        write_result(f, y_pred_test)
+    with open(args.metrics_out, 'w') as f:
+        f.write("error(train): " + str(f"{tr_err:.6f}" + "\n"))
+        f.write("error(test): " + str(f"{test_err:.6f}" + "\n"))
+
+
+    # VERIFICATION 
+    
+
+
+
+
